2f8acff3fe9453b49b462fb44d2acb3576e51f5d
Raphaël Tisba <raphaeltisba@yahoo.fr>
2021-04-06 16:10:31 +0200

    Ajout du squelette sur git

---
+++ modeles/modele.php
@@ -0,0 +1,6 @@
+<?php
+function getBDD()
+{
+    // INITIALISATION DE LA CONNEXION A LA BDD
+    return new PDO('mysql:host=localhost;dbname=gestion;charset=UTF8', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+}
