0408fa6ebabea033e5d174ed7b3290df1d8ee401
tcavaille-coll <theocavaillecoll@gmail.com>
2021-04-12 14:45:30 +0200

    modif

--- modeles/utilisateurs.php
+++ modeles/utilisateurs.php
@@ -1,5 +1,58 @@
 <?php
 
+class Utilisateur{
+    public $email;
+    public $pseudo;
+    public $mdp;
+    
+    public function __construct($email, $pseudo, $mdp)
+    {
+        $this->email=$email;
+        $this->pseudo=$pseudo;
+        $this->mdp=$mdp;
+    }
+    
+
+    public function creerUtilisateur($email, $pseudo, $mdp)
+{
+    
+    $requete = getBDD()->prepare("INSERT INTO utilisateurs(email, pseudo, mdp)
+    VALUES(?, ?, ?)");
+    $requete->execute([$email, $pseudo, $mdp]);
+    return true;
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function recupererInfosConnexion($pseudo)
+{
+    $requete = getBDD()->prepare("SELECT idUtilisateur, pseudo, mdp, idRole FROM utilisateurs WHERE pseudo = ?");
+    $requete->execute([$pseudo]);
+    return $requete;
+}
 function recupererUtilisateurs()
 {
     $requete = getBDD()->prepare("SELECT * FROM utilisateurs");
@@ -21,46 +74,11 @@ function recupererUtilisateur($idUtilisateur)
     return $requete->fetch(PDO::FETCH_ASSOC);
 }
 
-function mailUnique($email)
-{
-    $requete = getBDD()->prepare("SELECT email FROM utilisateurs WHERE email = ?");
-    $requete->execute([$email]);
-    return $requete;
-}
+
 
 function recupererEmail($email)
 {
     $requete = getBDD()->prepare("SELECT email FROM utilisateurs WHERE email = ?");
     $requete->execute([$email]);
-    return $requete;
-}
-
-function creerUtilisateur($email, $pseudo, $mdp)
-{
-    $requete = getBDD()->prepare("INSERT INTO utilisateurs(email, pseudo, mdp)
-    VALUES(?, ?, ?)");
-    $requete->execute([$email, $pseudo, $mdp]);
-    return true;
-}
-
-function modifierUtilisateur($idUtilisateur, $email, $pseudo, $idRole)
-{
-    $requete = getBDD()->prepare("UPDATE utilisateurs SET email = ?, pseudo = ?, idRole = ?  
-    WHERE idUtilisateur = ?");
-    $requete->execute([$email, $pseudo, $idRole, $idUtilisateur]);
-    return true;
-}
-
-function supprimerUtilisateur($idUtilisateur)
-{
-    $requete = getBDD()->prepare("DELETE FROM utilisateurs 
-    WHERE idUtilisateur = ?");
-    $requete->execute([$idUtilisateur]);
-    return true;
-}
-function recupererInfosConnexion($pseudo)
-{
-    $requete = getBDD()->prepare("SELECT idUtilisateur, pseudo, mdp, idRole FROM utilisateurs WHERE pseudo = ?");
-    $requete->execute([$pseudo]);
     return $requete;
 }
